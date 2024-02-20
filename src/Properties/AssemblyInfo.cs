3eba044687aceb9cd5ad3630bd44fad0adff289d
unknown <atasoy91@gmail.com>
2024-02-20 09:35:44 +0300

    Updates to prevent crashing while processing corrupted image files

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CNNFET")]
-[assembly: AssemblyCopyright("Copyright © Huseyin Atasoy 2022")]
+[assembly: AssemblyCopyright("Copyright © Huseyin Atasoy 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
